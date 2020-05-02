128b3c4c2a7fd8d30911d76ad915d3d10b03fb03
sky <sai.kumar@paralaxiom.com>
2020-05-02 21:24:13 +0530

    changed iteration variable names

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,3 +1,6 @@
+# no imports
+
+
 def bubble_sort(arr):
     arr_len = len(arr)
     for i in range(arr_len-1):
@@ -6,18 +9,18 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
+
 if __name__ == '__main__':
     num = input("Enter the list of integers with space between them:\n")
     input_list = num.split()
     int_input_list = list()
     try:
-        for i in input_list:
-            j = float(i)
-            int_input_list.append(j)
+        for input_element in input_list:
+            list_item = float(input_element)
+            int_input_list.append(list_item)
     except ValueError:
         print('only numbers allowed')
         exit()
     print(int_input_list)
     output = bubble_sort(int_input_list)
     print(output)
-
